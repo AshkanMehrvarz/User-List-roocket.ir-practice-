e47b54e781b0250dd2169f2e3773b81cfc997f06
Ashkan Mehrvarz <ashkanmehrvarzmac@gmail.com>
2022-05-05 00:40:53 +0430

    Add basic things [ inputs and "add user" button ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
   return (
     <div>
-      <p>hi</p>
+      <input type='text' placeholder='First Name' />
+      <input type='text' placeholder='Last Name' />
+      <input type='text' placeholder='Age' />
+      <input type='text' placeholder='City' />
+      <input type='text' placeholder='Email' />
+      <input type='text' placeholder='Phone Number' />
+      <input type='text' placeholder='Role' />
+      <button>Add User</button>
     </div>
   );
 }
