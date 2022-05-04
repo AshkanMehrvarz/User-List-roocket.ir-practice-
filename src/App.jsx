c7a9bfafee8075df31f707d79219b78d9d25a29a
Ashkan Mehrvarz <ashkanmehrvarzmac@gmail.com>
2022-05-05 00:56:50 +0430

    Basic Setup Of States  testting states]

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const addUser = () => {
+    setUsers([...users, user]);
+  };
   return (
     <div>
-      <input type='text' placeholder='First Name' />
+      <input
+        type='text'
+        placeholder='First Name'
+        onChange={(e) => setUser({ FirstName: e.target.value })}
+      />
       <input type='text' placeholder='Last Name' />
-      <input type='text' placeholder='Age' />
+      <input type='number' placeholder='Age' />
       <input type='text' placeholder='City' />
-      <input type='text' placeholder='Email' />
-      <input type='text' placeholder='Phone Number' />
+      <input type='email' placeholder='Email' />
+      <input type='number' placeholder='Phone Number' />
       <input type='text' placeholder='Role' />
-      <button>Add User</button>
+      <button onClick={addUser}>Add User</button>
+      {console.log(users)}
     </div>
   );
 }
