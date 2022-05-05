a6fccb58476cedd2d7186318bfe1165fc14e0b76
Ashkan Mehrvarz <ashkanmehrvarzmac@gmail.com>
2022-05-06 01:15:51 +0430

    ReCreate app with short and optimize codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,44 @@
-//:)
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [datas, setDatas] = useState([]);
+  const [newUserData, setNewUserData] = useState({});
+  const addNewUser = () => {
+    setDatas([...datas, newUserData]);
+  };
+  useEffect(() => {
+    if (datas.length > 0) {
+      console.log(datas);
+    }
+  }, [datas]);
+
+  return (
+    <div>
+      {[
+        "FirstName",
+        "LastName",
+        "Age",
+        "City",
+        "Admin",
+        "Role",
+        "Email",
+        "Address",
+      ].map((item, index) => {
+        return (
+          <input
+            key={index}
+            type='text'
+            placeholder={item}
+            onChange={(e) =>
+              setNewUserData({
+                ...newUserData,
+                [item]: e.target.value,
+              })
+            }
+          />
+        );
+      })}
+      <button onClick={addNewUser}>Add</button>
+    </div>
+  );
+}
