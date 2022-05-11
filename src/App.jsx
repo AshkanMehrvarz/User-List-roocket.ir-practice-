b1c6b723ff35abbb2903dd72f94b14e0ddb985c2
Ashkan Mehrvarz <ashkanmehrvarzmac@gmail.com>
2022-05-11 16:50:41 +0430

    All Codes Removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import AddNewUser from "./components/AddNewUser";
-import "./style.css";
-
 export default function App() {
-  return (
-    <>
-      {/* Header Section */}
-      <div className='Header'>
-        <h1>User List</h1>
-        <AddNewUser />
-      </div>
-
-      {/* Table Of Users */}
-    </>
-  );
+  return <div>App</div>;
 }
